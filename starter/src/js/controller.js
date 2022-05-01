8092b7d7390ca14da48ba9f09969c07a13a34b20
sssrgo-dev <sssrgo@yandex.ru>
2022-05-01 17:50:46 +0300

    Adden new welcome features

--- starter/src/js/controller.js
+++ starter/src/js/controller.js
@@ -132,6 +132,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecepies);
@@ -140,7 +144,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome!');
+  newFeature();
 };
 
 init();
